74212229eb8ecbf86f7f79f727ae3e712eb87339
RotterYkrn <sugaku.ohno@gmail.com>
2024-10-22 19:13:06 +0900

    016 カンニング

---
+++ memo/GCD.cpp
@@ -0,0 +1,15 @@
+long long GCD(long long a, long long b) {
+    if (b == 0) return a;
+    else return GCD(b, a % b);
+}
+
+long long extGCD(long long a, long long b, long long &x, long long &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    long long d = extGCD(b, a % b, y, x);
+    y -= a / b * x;
+    return d;
+}

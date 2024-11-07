031a2f7b1049554ec71356fc5196a73e4c72a52f
RotterYkrn <sugaku.ohno@gmail.com>
2024-11-07 10:25:35 +0900

    見た目改善

--- tmp/main.cpp
+++ tmp/main.cpp
@@ -26,16 +26,16 @@ using vpll = vector<pll>;
 
 #define OVERLOAD_MACRO(_1, _2, _3, name, ...) name
 // loop [begin,end)
-#define REP0(end) for (auto _ = decay_t<decltype(end)>{}; (_) != (end); ++(_))
-#define REP1(i, end) for (auto i = decay_t<decltype(end)>{}; (i) != (end); ++(i))
+#define REP0(end)           for (auto _ = decay_t<decltype(end)>{};      (_) != (end); ++(_))
+#define REP1(i, end)        for (auto i = decay_t<decltype(end)>{};      (i) != (end); ++(i))
 #define REP2(i, begin, end) for (auto i = decay_t<decltype(end)>{begin}; (i) != (end); ++(i))
 #define rep(...) OVERLOAD_MACRO(__VA_ARGS__, REP2, REP1, REP0)(__VA_ARGS__)
 // reveres loop [rend,rbegin)
-#define RREP1(i, rbegin) for (auto i = (rbegin-1); i >= 0; i--)
-#define RREP2(i, rbigin, rend) for (auto i = decay_t<decltype(rbigin)>{rend-1}; (i) >= (rbegin); i--)
+#define RREP1(i, rbegin)       for (auto i = (rbegin-1);                          (i) >= 0;        --(i))
+#define RREP2(i, rbigin, rend) for (auto i = decay_t<decltype(rbigin)>{rend - 1}; (i) >= (rbegin); --(i))
 #define rrep(...) OVERLOAD_MACRO(__VA_ARGS__, RREP2, RREP1)(__VA_ARGS__)
 // is in [l,r)
-#define INRANGE1(x, r) (0 <= x && x < r)
+#define INRANGE1(x, r)    (0 <= x && x < r)
 #define INRANGE2(x, l, r) (l <= x && x < r)
 #define ir(...) OVERLOAD_MACRO(__VA_ARGS__, INRANGE2, INRANGE1)(__VA_ARGS__)
 
@@ -46,11 +46,11 @@ using vpll = vector<pll>;
 #define el '\n'
 #define spa " "
 #define Yes cout << "Yes" << el
-#define No cout << "No" << el
+#define No  cout << "No" << el
 #define yes cout << "yes" << el
-#define no cout << "no" << el
+#define no  cout << "no" << el
 #define YES cout << "YES" << el
-#define NO cout << "NO" << el
+#define NO  cout << "NO" << el
 #define YESNO(bool) if(bool) { cout<<"YES"<<el; } else { cout<<"NO"<<el; }
 #define yesno(bool) if(bool) { cout<<"yes"<<el; } else { cout<<"no"<<el; }
 #define YesNo(bool) if(bool) { cout<<"Yes"<<el; } else { cout<<"No"<<el; }
@@ -65,12 +65,12 @@ const string ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const string abc = "abcdefghijklmnopqrstuvwxyz";
 
 // 4近傍、(一般的に)上右下左
-constexpr int dy[4] = {-1,0,1,0};
-constexpr int dx[4] = {0,1,0,-1};
+constexpr int dy[4] = {-1, 0, 1, 0};
+constexpr int dx[4] = { 0, 1, 0,-1};
 
 // 8方向 左上, 上, 右上, 右, 右下, 下, 左下, 左
-constexpr int dy8[8] = {-1,-1,-1,0,1,1,1,0};
-constexpr int dx8[8] = {-1,0,1,1,1,0,-1,-1};
+constexpr int dy8[8] = {-1,-1,-1, 0, 1, 1, 1, 0};
+constexpr int dx8[8] = {-1, 0, 1, 1, 1, 0,-1,-1};
 
 /***************************************
  mkvec<type>(n or {n1,n2,...}[, init])
@@ -89,7 +89,7 @@ auto mkvec(const int (&d)[n], const T& init = T{}) noexcept {
  pairとvectorを簡単に出力できるようにした
 ****************************************/
 template<class T1, class T2>
-ostream &operator<<(ostream &os, const pair<T1,T2> p) {
+ostream &operator<<(ostream &os, const pair<T1, T2> p) {
     os << "(" << p.first << "," << p.second << ")";
     return os;
 }
@@ -97,11 +97,11 @@ template<class T>
 ostream &operator<<(ostream &os, const vector<T> v) {
     if constexpr (is_same_v<T, string>) {
         for (int i = 0; i < (int)v.size(); i++) {
-            os << v[i] << (i+1!=(int)v.size() ? "\n" : "");
+            os << v[i] << (i + 1 != (int)v.size() ? "\n" : "");
         }
     } else {
         for (int i = 0; i < (int)v.size(); i++) {
-            os << v[i] << (i+1!=(int)v.size() ? spa : "");
+            os << v[i] << (i + 1 != (int)v.size() ? spa : "");
         }
     }
     return os;
@@ -109,7 +109,7 @@ ostream &operator<<(ostream &os, const vector<T> v) {
 template<class T>
 ostream &operator<<(ostream &os, const vector<vector<T>> v) {
     for (int i = 0; i < (int)v.size(); i++) {
-        os << v[i] << (i+1!=(int)v.size() ? "\n" : "");
+        os << v[i] << (i + 1 != (int)v.size() ? "\n" : "");
     }
     return os;
 }
@@ -126,8 +126,8 @@ void input_cin(First& first, Rest&... rest) {
     cin >> first;
     input_cin(rest...); // recursive call using pack expansion syntax
 }
-#define inputi(...) int __VA_ARGS__; input_cin(__VA_ARGS__);
-#define inputll(...) ll __VA_ARGS__; input_cin(__VA_ARGS__);
+#define inputi(...)    int __VA_ARGS__; input_cin(__VA_ARGS__);
+#define inputll(...)    ll __VA_ARGS__; input_cin(__VA_ARGS__);
 #define inputs(...) string __VA_ARGS__; input_cin(__VA_ARGS__);
 template <class T>
 inline auto inputv(const int d) {
@@ -186,38 +186,30 @@ inline bool chmin(T1 &a, const T2 b) noexcept {
 /*************************
  vectorの最大値、最小値
 **************************/
-template<template<class, class> class T1, class T2, class T3>
-inline T2 max(const T1<T2,T3> v) noexcept {
+template<class T>
+inline T max(const vector<T> v) noexcept {
     return *max_element(all(v));
 }
-template<template<class, class> class T1, class T2, class T3>
-inline T2 min(const T1<T2,T3> v) noexcept {
+template<class T>
+inline T min(const vector<T> v) noexcept {
     return *min_element(all(v));
 }
 /***************************
  2次元vectorの最大値、最小値
 ****************************/
-template<template<class, class> class T1, template<class, class> class T2, class T3, class T4, class T5>
-T4 max(const T1<T2<T4, T5>, T3> v) noexcept {
-    T4 maxValue;
+template<class T>
+T max(const vector<vector<T>> v) noexcept {
+    T maxValue = numeric_limits<T>::lowest();
     for (auto iter = v.begin(); iter < v.end(); ++iter) {
-        if (iter == v.begin()) {
-            maxValue = max(*iter);
-        } else {
-            chmax(maxValue, max(*iter));
-        }
+        chmax(maxValue, max(*iter));
     }
     return maxValue;
 }
-template<template<class, class> class T1, template<class, class> class T2, class T3, class T4, class T5>
-T4 min(const T1<T2<T4, T5>, T3> v) noexcept {
-    T4 minValue;
+template<class T>
+T min(const vector<vector<T>> v) noexcept {
+    T minValue = numeric_limits<T>::max();
     for (auto iter = v.begin(); iter < v.end(); ++iter) {
-        if (iter == v.begin()) {
-            minValue = min(*iter);
-        } else {
-            chmin(minValue, min(*iter));
-        }
+        chmin(minValue, min(*iter));
     }
     return minValue;
 }

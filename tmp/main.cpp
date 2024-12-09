f673b775212f8e51fb22b2bf60832dfda4b6ddd7
RotterYkrn <sugaku.ohno@gmail.com>
2024-12-10 00:50:20 +0900

    up tmp

--- tmp/main.cpp
+++ tmp/main.cpp
@@ -20,8 +20,8 @@ struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); } }init;
 
 using ll = long long;
 using ull = unsigned long long;
-using pii = pair<int,int>;
-using pll = pair<ll,ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 using vi = vector<int>;
 using vll = vector<ll>;
 using vpii = vector<pii>;
@@ -50,7 +50,7 @@ using vpll = vector<pll>;
 #define mp make_pair
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
-#define el '\n'
+#define el "\n"
 #define spa " "
 #define Yes cout << "Yes" << el
 #define No  cout << "No" << el
@@ -79,15 +79,22 @@ constexpr int dx[4] = { 0, 1, 0,-1};
 constexpr int dy8[8] = {-1,-1,-1, 0, 1, 1, 1, 0};
 constexpr int dx8[8] = {-1, 0, 1, 1, 1, 0,-1,-1};
 
+template <typename T>
+struct is_vector : std::false_type { };
+template <typename T, typename Allocator>
+struct is_vector<std::vector<T, Allocator>> : std::true_type { };
+template <typename T>
+inline constexpr bool is_vector_v = is_vector<T>::value;
+
 /***************************************
  mkvec<type>(n or {n1,n2,...}[, init])
 ****************************************/
 template<class T>
-inline auto mkvec(const auto d, const T& init = T{}) noexcept {
+inline auto mkvec(const auto d, const T &init = T{}) noexcept {
     return vector<T>(d, init);
 }
 template<class T, size_t n, size_t idx = 0>
-auto mkvec(const auto (&d)[n], const T& init = T{}) noexcept {
+auto mkvec(const auto (&d)[n], const T &init = T{}) noexcept {
     if constexpr (idx < n - 1) return vector(d[idx], mkvec<T, n, idx + 1>(d, init));
     else return mkvec<T>(d[idx], init);
 }
@@ -103,20 +110,20 @@ ostream &operator<<(ostream &os, const pair<T1, T2> p) {
 template<class T>
 ostream &operator<<(ostream &os, const vector<T> v) {
     if constexpr (is_same_v<T, string>) {
-        for (int i = 0; i < (int)v.size(); i++) {
-            os << v[i] << (i + 1 != (int)v.size() ? "\n" : "");
+        for (auto it = v.begin(); it != v.end(); ++it) {
+            os << *it << (it + 1 != v.end() ? el : "");
         }
     } else {
-        for (int i = 0; i < (int)v.size(); i++) {
-            os << v[i] << (i + 1 != (int)v.size() ? spa : "");
+        for (auto it = v.begin(); it != v.end(); ++it) {
+            os << *it << (it + 1 != v.end() ? spa : "");
         }
     }
     return os;
 }
 template<class T>
 ostream &operator<<(ostream &os, const vector<vector<T>> v) {
-    for (int i = 0; i < (int)v.size(); i++) {
-        os << v[i] << (i + 1 != (int)v.size() ? "\n" : "");
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << *it << (it + 1 != v.end() ? el : "");
     }
     return os;
 }
@@ -125,11 +132,11 @@ ostream &operator<<(ostream &os, const vector<vector<T>> v) {
  複数の標準入力
 ****************/
 template <class T>
-inline void input_cin(T& val) {
+inline void input_cin(T &val) {
     cin >> val;
 }
 template <class First, class... Rest>
-void input_cin(First& first, Rest&... rest) {
+void input_cin(First &first, Rest&... rest) {
     cin >> first;
     input_cin(rest...); // recursive call using pack expansion syntax
 }
@@ -139,19 +146,19 @@ void input_cin(First& first, Rest&... rest) {
 template <class T>
 inline auto inputv(const auto d) {
     vector<T> vec(d);
-    for (int i = 0; i < d; i++) {
-        cin >> vec[i];
+    for (auto &i : vec) {
+        cin >> i;
     }
     return vec;
 }
-template<class T, size_t n, size_t idx = 0>
-auto inputv(const auto (&d)[n]) noexcept {
+template<class T, class D, size_t n, size_t idx = 0>
+auto inputv(const D(&d)[n]) noexcept {
     if constexpr (idx < n - 1) {
-        int d_make[n - idx];
+        D d_make[n - idx];
         copy(begin(d) + idx, end(d), begin(d_make));
         auto vec = mkvec<T>(d_make);
-        for (int i = 0; i < (int)vec.size(); i++) {
-            vec[i] = inputv<T, n, idx + 1>(d);
+        for (auto &i : vec) {
+            i = inputv<T, D, n, idx + 1>(d);
         }
         return vec;
     } else {
@@ -165,14 +172,20 @@ auto inputv(const auto (&d)[n]) noexcept {
 inline void print() {
     cout << el;
 }
-template <class T>
-inline void print(const T& t) {
+template <bool new_line = false, class T>
+inline void print(const T &t) {
+    if (new_line && is_vector_v<T>) {
+        cout << el;
+    }
     cout << t << el;
 }
-template <class First, class... Rest>
-void print(const First& first, const Rest&... rest) {
-    cout << first << " ";
-    print(rest...); // recursive call using pack expansion syntax
+template <bool new_line = false, class First, class... Rest>
+void print(const First &first, const Rest&... rest) {
+    if (new_line && is_vector_v<First>) {
+        cout << el;
+    }
+    cout << first << (is_vector_v<First> ? el : " ");
+    print<!is_vector_v<First>>(rest...); // recursive call using pack expansion syntax
 }
 
 /*************************
@@ -181,7 +194,7 @@ void print(const First& first, const Rest&... rest) {
 template<class T1, class T2>
 inline bool chmax(T1 &a, const T2 b) noexcept {
     bool compare = a < b;
-    if(compare) a = b;
+    if (compare) a = b;
     return compare;
 }
 template<class T1, class T2>
@@ -207,16 +220,16 @@ inline T min(const vector<T> v) noexcept {
 template<class T>
 T max(const vector<vector<T>> v) noexcept {
     T maxValue = numeric_limits<T>::lowest();
-    for (auto iter = v.begin(); iter < v.end(); ++iter) {
-        chmax(maxValue, max(*iter));
+    for (const auto &i : v) {
+        chmax(maxValue, max(i));
     }
     return maxValue;
 }
 template<class T>
 T min(const vector<vector<T>> v) noexcept {
     T minValue = numeric_limits<T>::max();
-    for (auto iter = v.begin(); iter < v.end(); ++iter) {
-        chmin(minValue, min(*iter));
+    for (const auto &i : v) {
+        chmin(minValue, min(i));
     }
     return minValue;
 }
@@ -227,7 +240,7 @@ T min(const vector<vector<T>> v) noexcept {
 
 
 int main() {
-    
+
 
     return 0;
 }

f39b8cbd4e5f89969caf778a40ef19d03ccbce75
RotterYkrn <sugaku.ohno@gmail.com>
2025-01-02 00:56:27 +0900

    abc roller

--- abc/abc351~375/abc352/e/main.cpp
+++ abc/abc351~375/abc352/e/main.cpp
@@ -4,9 +4,12 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+
 #if __has_include(<atcoder/all>)
 #include <atcoder/all>
 using namespace atcoder;
+using mint = modint998244353;
+// using mint = modint1000000007;
 #endif
 
 // おまじない
@@ -16,48 +19,46 @@ using namespace atcoder;
 struct Init { Init() { ios::sync_with_stdio(0); cin.tie(0); } }init;
 
 using ll = long long;
-using ull = unsigned long long;
-using pii = pair<int,int>;
-using pll = pair<ll,ll>;
+using pii = pair<int, int>;
+using pil = pair<int, ll>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
 using vi = vector<int>;
-using vll = vector<ll>;
-using vpii = vector<pii>;
-using vpll = vector<pll>;
+using vl = vector<ll>;
 
 #define OVERLOAD_MACRO(_1, _2, _3, name, ...) name
 // loop [begin,end)
-#define REP0(end)           while(end--)
+#define REP0(end)           for (auto _ = decay_t<decltype(end)>{};      (_) != (end); ++(_))
 #define REP1(i, end)        for (auto i = decay_t<decltype(end)>{};      (i) != (end); ++(i))
 #define REP2(i, begin, end) for (auto i = decay_t<decltype(end)>{begin}; (i) != (end); ++(i))
 #define rep(...) OVERLOAD_MACRO(__VA_ARGS__, REP2, REP1, REP0)(__VA_ARGS__)
 // reveres loop [rend,rbegin)
-#define RREP1(i, rbegin)       for (auto i = (rbegin-1);                          (i) >= 0;        --(i))
-#define RREP2(i, rbigin, rend) for (auto i = decay_t<decltype(rbigin)>{rend - 1}; (i) >= (rbegin); --(i))
+#define RREP1(i, rbegin)       for (auto i = (rbegin - 1); (i) >= decay_t<decltype(rbegin)>{};     --(i))
+#define RREP2(i, rbegin, rend) for (auto i = (rbegin - 1); (i) >= decay_t<decltype(rbegin)>{rend}; --(i))
 #define rrep(...) OVERLOAD_MACRO(__VA_ARGS__, RREP2, RREP1)(__VA_ARGS__)
 // is in [l,r)
-#define INRANGE1(x, r)    (0 <= x && x < r)
-#define INRANGE2(x, l, r) (l <= x && x < r)
-#define ir(...) OVERLOAD_MACRO(__VA_ARGS__, INRANGE2, INRANGE1)(__VA_ARGS__)
+#define IN_RANGE1(x, r)    (0 <= x && x < r)
+#define IN_RANGE2(x, l, r) (l <= x && x < r)
+#define ir(...) OVERLOAD_MACRO(__VA_ARGS__, IN_RANGE2, IN_RANGE1)(__VA_ARGS__)
+// from range to iterator [left, right)
+#define RANGE_TO_ITERATOR1(arr, left) arr.begin() + (left), arr.end()
+#define RANGE_TO_ITERATOR2(arr, left, right) arr.begin() + (left), arr.begin() + (right)
+#define rtoit(...) OVERLOAD_MACRO(__VA_ARGS__, RANGE_TO_ITERATOR2, RANGE_TO_ITERATOR1)(__VA_ARGS__)
 
 #define pb push_back
 #define mp make_pair
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-#define el '\n'
-#define spa " "
-#define Yes cout << "Yes" << el
-#define No  cout << "No" << el
-#define yes cout << "yes" << el
-#define no  cout << "no" << el
-#define YES cout << "YES" << el
-#define NO  cout << "NO" << el
-#define YESNO(bool) if(bool) { cout<<"YES"<<el; } else { cout<<"NO"<<el; }
-#define yesno(bool) if(bool) { cout<<"yes"<<el; } else { cout<<"no"<<el; }
-#define YesNo(bool) if(bool) { cout<<"Yes"<<el; } else { cout<<"No"<<el; }
-#define eps (1e-10)
-#define Equals(a,b) (fabs((a) - (b)) < eps)
+#define all(container) container.begin(), container.end()
+#define rall(container) container.rbegin(), container.rend()
+#define YES cout << "YES\n"
+#define NO  cout << "NO\n"
+#define Yes cout << "Yes\n"
+#define No  cout << "No\n"
+#define yes cout << "yes\n"
+#define no  cout << "no\n"
+#define YESNO(bool) if(bool) YES; else NO
+#define YesNo(bool) if(bool) Yes; else No
+#define yesno(bool) if(bool) yes; else no
 #define isNum(s) all_of(all(s), [](char c){ return isdigit(c); })
-#define debug(x) cerr << #x << " = " << x << el
 
 constexpr int inf = 1073741823;
 constexpr ll infl = 1LL << 60;
@@ -66,50 +67,57 @@ const string abc = "abcdefghijklmnopqrstuvwxyz";
 
 // 4近傍、(一般的に)上右下左
 constexpr int dy[4] = {-1, 0, 1, 0};
-constexpr int dx[4] = { 0, 1, 0,-1};
+constexpr int dx[4] = {0, 1, 0,-1};
 
 // 8方向 左上, 上, 右上, 右, 右下, 下, 左下, 左
 constexpr int dy8[8] = {-1,-1,-1, 0, 1, 1, 1, 0};
 constexpr int dx8[8] = {-1, 0, 1, 1, 1, 0,-1,-1};
 
+template <class T>
+struct is_container : false_type { };
+template<template <class...> class Container, class T>
+struct is_container<Container<T>> : std::true_type { };
+template <class T>
+inline constexpr bool is_container_v = is_container<T>::value;
+
 /***************************************
  mkvec<type>(n or {n1,n2,...}[, init])
 ****************************************/
 template<class T>
-inline auto mkvec(const int d, const T& init = T{}) noexcept {
+inline auto mkvec(const auto d, const T &init = T{}) noexcept {
     return vector<T>(d, init);
 }
 template<class T, size_t n, size_t idx = 0>
-auto mkvec(const int (&d)[n], const T& init = T{}) noexcept {
+auto mkvec(const auto (&d)[n], const T &init = T{}) noexcept {
     if constexpr (idx < n - 1) return vector(d[idx], mkvec<T, n, idx + 1>(d, init));
     else return mkvec<T>(d[idx], init);
 }
 
 /***************************************
- pairとvectorを簡単に出力できるようにした
+ pairとコンテナを簡単に出力できるようにした
 ****************************************/
-template<class T1, class T2>
-ostream &operator<<(ostream &os, const pair<T1, T2> p) {
+template<class First, class Second>
+ostream &operator<<(ostream &os, const pair<First, Second> &p) {
     os << "(" << p.first << "," << p.second << ")";
     return os;
 }
-template<class T>
-ostream &operator<<(ostream &os, const vector<T> v) {
+template<template <class...> class Container, class T>
+ostream &operator<<(ostream &os, const Container<T> v) {
     if constexpr (is_same_v<T, string>) {
-        for (int i = 0; i < (int)v.size(); i++) {
-            os << v[i] << (i + 1 != (int)v.size() ? "\n" : "");
+        for (auto it = v.begin(); it != v.end(); ++it) {
+            os << *it << (next(it) != v.end() ? "\n" : "");
         }
     } else {
-        for (int i = 0; i < (int)v.size(); i++) {
-            os << v[i] << (i + 1 != (int)v.size() ? spa : "");
+        for (auto it = v.begin(); it != v.end(); ++it) {
+            os << *it << (next(it) != v.end() ? " " : "");
         }
     }
     return os;
 }
-template<class T>
-ostream &operator<<(ostream &os, const vector<vector<T>> v) {
-    for (int i = 0; i < (int)v.size(); i++) {
-        os << v[i] << (i + 1 != (int)v.size() ? "\n" : "");
+template<template <class...> class Container1, template <class...> class Container2, class T>
+ostream &operator<<(ostream &os, const Container1<Container2<T>> v) {
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        os << *it << (next(it) != v.end() ? "\n" : "");
     }
     return os;
 }
@@ -118,33 +126,39 @@ ostream &operator<<(ostream &os, const vector<vector<T>> v) {
  複数の標準入力
 ****************/
 template <class T>
-inline void input_cin(T& val) {
+inline void input_cin(T &val) {
     cin >> val;
 }
 template <class First, class... Rest>
-void input_cin(First& first, Rest&... rest) {
+void input_cin(First &first, Rest&... rest) {
     cin >> first;
-    input_cin(rest...); // recursive call using pack expansion syntax
+    input_cin(rest...);
 }
 #define inputi(...)    int __VA_ARGS__; input_cin(__VA_ARGS__);
 #define inputll(...)    ll __VA_ARGS__; input_cin(__VA_ARGS__);
+#define inputc(...)   char __VA_ARGS__; input_cin(__VA_ARGS__);
 #define inputs(...) string __VA_ARGS__; input_cin(__VA_ARGS__);
+template<class First, class Second>
+istream &operator>>(istream &in, pair<First, Second> &p) {
+    in >> p.first >> p.second;
+    return in;
+}
 template <class T>
-inline auto inputv(const int d) {
+inline auto inputv(const auto d) {
     vector<T> vec(d);
-    for (int i = 0; i < d; i++) {
-        cin >> vec[i];
+    for (auto &i : vec) {
+        cin >> i;
     }
     return vec;
 }
-template<class T, size_t n, size_t idx = 0>
-auto inputv(const int (&d)[n]) noexcept {
+template<class T, class D, size_t n, size_t idx = 0>
+auto inputv(const D(&d)[n]) noexcept {
     if constexpr (idx < n - 1) {
-        int d_make[n - idx];
+        D d_make[n - idx];
         copy(begin(d) + idx, end(d), begin(d_make));
         auto vec = mkvec<T>(d_make);
-        for (int i = 0; i < (int)vec.size(); i++) {
-            vec[i] = inputv<T, n, idx + 1>(d);
+        for (auto &i : vec) {
+            i = inputv<T, D, n, idx + 1>(d);
         }
         return vec;
     } else {
@@ -156,16 +170,22 @@ auto inputv(const int (&d)[n]) noexcept {
  Pythonのprintみたいなやつ
 **************************/
 inline void print() {
-    cout << el;
+    cout << "\n";
 }
-template <class T>
-inline void print(const T& t) {
-    cout << t << el;
+template <bool new_line = false, class T>
+inline void print(const T &t) {
+    if (new_line && is_container_v<T>) {
+        cout << "\n";
+    }
+    cout << t << "\n";
 }
-template <class First, class... Rest>
-void print(const First& first, const Rest&... rest) {
-    cout << first << " ";
-    print(rest...); // recursive call using pack expansion syntax
+template <bool new_line = false, class First, class... Rest>
+void print(const First &first, const Rest&... rest) {
+    if (new_line && is_container_v<First>) {
+        cout << "\n";
+    }
+    cout << first << (is_container_v<First> ? "\n" : " ");
+    print<!is_container_v<First>>(rest...);
 }
 
 /*************************
@@ -174,7 +194,7 @@ void print(const First& first, const Rest&... rest) {
 template<class T1, class T2>
 inline bool chmax(T1 &a, const T2 b) noexcept {
     bool compare = a < b;
-    if(compare) a = b;
+    if (compare) a = b;
     return compare;
 }
 template<class T1, class T2>
@@ -200,16 +220,16 @@ inline T min(const vector<T> v) noexcept {
 template<class T>
 T max(const vector<vector<T>> v) noexcept {
     T maxValue = numeric_limits<T>::lowest();
-    for (auto iter = v.begin(); iter < v.end(); ++iter) {
-        chmax(maxValue, max(*iter));
+    for (const auto &i : v) {
+        chmax(maxValue, max(i));
     }
     return maxValue;
 }
 template<class T>
 T min(const vector<vector<T>> v) noexcept {
     T minValue = numeric_limits<T>::max();
-    for (auto iter = v.begin(); iter < v.end(); ++iter) {
-        chmin(minValue, min(*iter));
+    for (const auto &i : v) {
+        chmin(minValue, min(i));
     }
     return minValue;
 }
@@ -220,7 +240,35 @@ T min(const vector<vector<T>> v) noexcept {
 
 
 int main() {
-    
+    inputi(N, M);
+    auto CA = mkvec<pair<ll, vi>>(M);
+    rep(i, M) {
+        inputi(K);
+        cin >> CA[i].first;
+        CA[i].second = mkvec<int>(K);
+        rep(j, K) {
+            cin >> CA[i].second[j];
+            CA[i].second[j]--;
+        }
+    }
+    sort(all(CA));
+
+    dsu uf(N);
+    ll ans = 0;
+    int group = N;
+
+    for (auto [C, A] : CA) {
+        rep(k, A.size() - 1) {
+            if (uf.same(A[k], A[k + 1])) continue;
+            ans += C;
+            uf.merge(A[k], A[k + 1]);
+            group--;
+        }
+        if (group == 1) break;
+    }
+
+    if (group == 1) print(ans);
+    else print(-1);
 
     return 0;
 }
